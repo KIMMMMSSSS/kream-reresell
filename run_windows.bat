96f21d3103114c20ddfea97a94e38ba62fe41e58
KIMMMMSSSS <troy0923@hanyang.ac.kr>
2026-09-19 14:26:46 +0900

    Use top 100 products in Windows launcher

--- run_windows.bat
+++ run_windows.bat
@@ -7,8 +7,8 @@ if not exist .venv\Scripts\python.exe (
   exit /b 1
 )
 
-set /p MAX_PRODUCTS=How many ranking products should be checked? (example 20): 
-if "%MAX_PRODUCTS%"=="" set MAX_PRODUCTS=20
+set /p MAX_PRODUCTS=How many ranking products should be checked? (default 100): 
+if "%MAX_PRODUCTS%"=="" set MAX_PRODUCTS=100
 
 call .venv\Scripts\activate.bat
 python main.py --max-products %MAX_PRODUCTS%
